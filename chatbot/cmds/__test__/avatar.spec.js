b77f5f61a183b7089f30862bafafd293a3836b72
Nick Grayson <intercomman14@gmail.com>
2020-11-29 17:29:40 -0800

    added test case for avatar command

---
+++ chatbot/cmds/__test__/avatar.spec.js
@@ -0,0 +1,44 @@
+import { run } from '../avatar.js'
+describe('Bot cmds - avatar', () => {
+  const mockUrl = 'HTTPS://gromlet.org/TEE_HEE_SEKRIT_STRING.jif'
+  let mockDelete, mockMessage
+  beforeEach(() => {
+    mockDelete = jest.fn()
+    mockMessage = {
+      channel: {
+        send: jest.fn(() => {
+          return {
+            delete: mockDelete
+          }
+        })
+      },
+      author: {
+        displayAvatarURL: mockUrl
+      }
+    }
+  })
+
+  it('should delete the message when it is done.', async () => {
+    await run(undefined, mockMessage)
+    expect(mockDelete).toHaveBeenCalled()
+  })
+
+  it('should send a message with a file to the channel', async () => {
+    await run(undefined, mockMessage)
+    expect(mockMessage.channel.send).toHaveBeenCalledTimes(2)
+
+    expect(mockMessage.channel.send).toHaveBeenNthCalledWith(
+      1,
+      'generating avatar...'
+    )
+
+    expect(mockMessage.channel.send).toHaveBeenNthCalledWith(2, {
+      files: [
+        {
+          attachment: mockUrl,
+          name: 'avatar.png'
+        }
+      ]
+    })
+  })
+})
