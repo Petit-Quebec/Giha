e36dc7c4d28b137e047e2ae6e0c28560b7a1c5a1
egeels <54962253+egeels@users.noreply.github.com>
2020-12-02 22:14:01 -0800

    Tests for !stats (#8)

---
+++ chatbot/cmds/__test__/stats.spec.js
@@ -0,0 +1,68 @@
+import { run } from '../stats.js'
+import { getHeroById } from '../../../Giha/heroManager.js'
+jest.mock('../../../Giha/heroManager.js')
+
+describe('Bot cmds - stats', () => {
+    let mockMessage
+    let mockEdit
+
+    beforeEach(() => {
+        mockEdit = jest.fn()
+        mockMessage = {
+            author: {
+                id: 1
+            },
+            channel: {
+                send: jest.fn(() => {
+                    return {
+                        edit: mockEdit
+                    }
+                })
+            }
+        }
+    })
+
+    it('should tell you if there is no matching hero', async () => {
+
+        await run(null, mockMessage, ['Dilbert'])
+        expect(mockMessage.channel.send).toHaveBeenCalledWith(
+            'performing function...'
+        )
+        expect(mockEdit).toHaveBeenCalledWith(
+            'There is no hero matching this name.'
+        )
+    })
+
+    it('should tell you if you have no hero', async () => {
+        await run(null, mockMessage, [])
+        expect(mockMessage.channel.send).toHaveBeenCalledWith(
+            'performing function...'
+        )
+        expect(mockEdit).toHaveBeenCalledWith(
+            'This command requires a hero! Create a hero with !rise.'
+        )
+    })
+
+    
+    it('should print stats if you have a hero', async () => {
+
+        getHeroById.mockReturnValue({
+            statsBlob : jest.fn(() => 'Mock Hero Stats Blob')
+        })
+
+        await run(null, mockMessage, [])
+        expect(mockMessage.channel.send).toHaveBeenCalledWith(
+            'performing function...'
+        )
+        expect(mockEdit).toHaveBeenCalledWith('Mock Hero Stats Blob')
+    })
+
+    it('should catch if there is an issue', async () => {
+        getHeroById.mockReturnValue('String')
+        await run(null, mockMessage, [])
+        expect(mockMessage.channel.send).toHaveBeenCalledWith(
+            'performing function...'
+        )
+        expect(mockEdit).toHaveBeenCalled()
+    })
+})
