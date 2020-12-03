cb64b19a3e58ac776f5dc524787ae832c8a8d216
Rachel Fisher <rachelfish53@gmail.com>
2020-12-02 22:30:50 -0800

    prettier

--- chatbot/cmds/__test__/stats.spec.js
+++ chatbot/cmds/__test__/stats.spec.js
@@ -3,66 +3,63 @@ import { getHeroById } from '../../../Giha/heroManager.js'
 jest.mock('../../../Giha/heroManager.js')
 
 describe('Bot cmds - stats', () => {
-    let mockMessage
-    let mockEdit
+  let mockMessage
+  let mockEdit
 
-    beforeEach(() => {
-        mockEdit = jest.fn()
-        mockMessage = {
-            author: {
-                id: 1
-            },
-            channel: {
-                send: jest.fn(() => {
-                    return {
-                        edit: mockEdit
-                    }
-                })
-            }
-        }
-    })
-
-    it('should tell you if there is no matching hero', async () => {
-
-        await run(null, mockMessage, ['Dilbert'])
-        expect(mockMessage.channel.send).toHaveBeenCalledWith(
-            'performing function...'
-        )
-        expect(mockEdit).toHaveBeenCalledWith(
-            'There is no hero matching this name.'
-        )
-    })
-
-    it('should tell you if you have no hero', async () => {
-        await run(null, mockMessage, [])
-        expect(mockMessage.channel.send).toHaveBeenCalledWith(
-            'performing function...'
-        )
-        expect(mockEdit).toHaveBeenCalledWith(
-            'This command requires a hero! Create a hero with !rise.'
-        )
-    })
-
-    
-    it('should print stats if you have a hero', async () => {
-
-        getHeroById.mockReturnValue({
-            statsBlob : jest.fn(() => 'Mock Hero Stats Blob')
+  beforeEach(() => {
+    mockEdit = jest.fn()
+    mockMessage = {
+      author: {
+        id: 1
+      },
+      channel: {
+        send: jest.fn(() => {
+          return {
+            edit: mockEdit
+          }
         })
+      }
+    }
+  })
 
-        await run(null, mockMessage, [])
-        expect(mockMessage.channel.send).toHaveBeenCalledWith(
-            'performing function...'
-        )
-        expect(mockEdit).toHaveBeenCalledWith('Mock Hero Stats Blob')
+  it('should tell you if there is no matching hero', async () => {
+    await run(null, mockMessage, ['Dilbert'])
+    expect(mockMessage.channel.send).toHaveBeenCalledWith(
+      'performing function...'
+    )
+    expect(mockEdit).toHaveBeenCalledWith(
+      'There is no hero matching this name.'
+    )
+  })
+
+  it('should tell you if you have no hero', async () => {
+    await run(null, mockMessage, [])
+    expect(mockMessage.channel.send).toHaveBeenCalledWith(
+      'performing function...'
+    )
+    expect(mockEdit).toHaveBeenCalledWith(
+      'This command requires a hero! Create a hero with !rise.'
+    )
+  })
+
+  it('should print stats if you have a hero', async () => {
+    getHeroById.mockReturnValue({
+      statsBlob: jest.fn(() => 'Mock Hero Stats Blob')
     })
 
-    it('should catch if there is an issue', async () => {
-        getHeroById.mockReturnValue('String')
-        await run(null, mockMessage, [])
-        expect(mockMessage.channel.send).toHaveBeenCalledWith(
-            'performing function...'
-        )
-        expect(mockEdit).toHaveBeenCalled()
-    })
+    await run(null, mockMessage, [])
+    expect(mockMessage.channel.send).toHaveBeenCalledWith(
+      'performing function...'
+    )
+    expect(mockEdit).toHaveBeenCalledWith('Mock Hero Stats Blob')
+  })
+
+  it('should catch if there is an issue', async () => {
+    getHeroById.mockReturnValue('String')
+    await run(null, mockMessage, [])
+    expect(mockMessage.channel.send).toHaveBeenCalledWith(
+      'performing function...'
+    )
+    expect(mockEdit).toHaveBeenCalled()
+  })
 })
