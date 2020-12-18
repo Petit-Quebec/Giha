cb3ba49bcd7d3de268358e8deb3e5b0a0aac1f23
Nick Grayson <intercomman14@gmail.com>
2020-12-17 20:16:05 -0800

    some instancelocation work, also lint-fix is arguing with prettier RACHEL!?!?

--- db/InstanceMap.js
+++ db/InstanceMap.js
@@ -6,55 +6,55 @@ import { parseLocations } from './instanceLocation.js'
 // const mongoose = db.mongoose
 
 let InstanceMap = class InstanceMap {
-    constructor ( mapName, topography )
-    {
-        topography.array.forEach( locationRow =>
-        {
-            locationRow.forEach( location =>
-            {
-                
-                if ( !( location instanceof instanceLocation ) )
-                throw "instance maps must be made of instancelocations!!"
-            })
-            
-        });
-        this.mapName = 'mapName'
-        this.topography = topography //an array of instanceLocations
+  constructor(mapName, topography) {
+    topography.array.forEach((locationRow) => {
+      locationRow.forEach((location) => {
+        if (!(location instanceof instanceLocation))
+          throw 'instance maps must be made of instancelocations!!'
+      })
+    })
+    this.mapName = 'mapName'
+    this.topography = topography //an array of instanceLocations
+    // this.zone // what zone it is in
+    //
 
-            log(`a new map called ${mapName}`)
-    }
-    getLocationAt(x,y) {
-        return this.topography[x,y]
-    }
+    log(`a new map called ${mapName}`)
+  }
+  getLocationAt(x, y) {
+    return this.topography[(x, y)]
+  }
 }
 
-const getTestInstance = () =>{
-    const testLocationStrings =
-        [
-            ['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N'],
-            ['N', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'N'],
-            ['N', 'S', ' ', ' ', 'S', 'S', ' ', ' ', ' ', ' ', 'S', 'S', 'S', 'S', 'S', ' ', ' ', 'E', 'S', 'S', 'S', 'S', 'N'],
-            ['N', 'S', ' ', 'E', ' ', ' ', ' ', 'S', 'S', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'S', 'S', 'S', 'S', 'S', 'S', 'N'],
-            ['N', 'S', ' ', ' ', ' ', ' ', 'S', 'S', ' ', ' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'E', ' ', 'S', 'N'],
-            ['N', 'S', 'S', ' ', ' ', 'S', 'S', ' ', ' ', 'S', 'S', 'S', 'E', ' ', ' ', 'S', 'S', ' ', ' ', ' ', ' ', 'S', 'N'],
-            ['N', 'S', 'S', 'S', ' ', 'S', ' ', ' ', 'S', 'S', 'S', 'S', ' ', ' ', ' ', ' ', ' ', ' ', 'S', ' ', ' ', 'S', 'N'],
-            ['N', 'S', 'S', ' ', ' ', 'S', ' ', ' ', ' ', ' ', 'S', 'S', 'S', 'S', 'S', 'S', 'S', ' ', 'S', 'S', ' ', 'S', 'N'],
-            ['N', 'S', 'S', ' ', 'v', 'S', 'E', 'v', 'v', ' ', ' ', ' ', ' ', 'S', 'S', 'S', ' ', ' ', 'S', ' ', ' ', 'S', 'N'],
-            ['N', 'S', ' ', ' ', 'v', 'S', 'v', 'v', 'v', 'v', 'v', ' ', ' ', ' ', ' ', ' ', ' ', 'S', 'S', ' ', 'S', 'S', 'N'],
-            ['N', 'S', ' ', ' ', ' ', 'S', 'v', 'v', 'v', 'v', 'v', 'v', 'v', 'v', 'v', 'S', 'S', 'S', 'S', ' ', 'S', 'S', 'N'],
-            ['N', 'S', ' ', 'S', ' ', 'S', 'S', 'v', 'v', 'v', 'v', 'v', 'v', 'v', 'v', 'S', ' ', ' ', ' ', ' ', ' ', 'S', 'N'],
-            ['N', 'S', ' ', 'S', ' ', ' ', 'S', 'S', 'S', 'v', 'v', 'v', 'v', 'v', 'S', ' ', ' ', ' ', 'E', ' ', ' ', 'S', 'N'],
-            ['N', 'S', ' ', 'S', 'S', ' ', ' ', 'S', 'S', 'S', 'S', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'S', 'N'],
-            ['N', 'S', ' ', ' ', 'S', 'S', ' ', ' ', 'S', 'S', ' ', ' ', ' ', ' ', ' ', ' ', 'S', ' ', ' ', ' ', ' ', 'S', 'N'],
-            ['N', 'S', ' ', ' ', 'E', 'S', 'S', ' ', ' ', ' ', ' ', ' ', ' ', 'S', 'S', 'S', 'S', 'S', ' ', ' ', 'S', 'S', 'N'],
-            ['N', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'T', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'N'],
-            ['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N']
-        ]
-    let testLocations = parseLocations(testLocationStrings)
-    let testInstance = new InstanceMap( 'Test Instance 0', testLocations )
-    return testInstance
+const getTestInstance = () => {
+  const testLocationStrings = [
+    'NNNNNNNNNNNNNNNNNNNNNNN',
+    'NSSSSSSSSSSSSSSSSSSSSSN',
+    'NS  SS    SSSSS  ESSSSN',
+    'NS E   SS       SSSSSSN',
+    'NS    SS  SSSSSSSSSE SN',
+    'NSS  SS  SSSE  SS    SN',
+    'NSSS S  SSSS      S  SN',
+    'NSS  S    SSSSSSS SS SN',
+    'NSS vSEvv    SSS  S  SN',
+    'NS  vSvvvvv      SS SSN',
+    'NS   SvvvvvvvvvSSSS SSN',
+    'NS S SSvvvvvvvvS     SN',
+    'NS S  SSSvvvvvS   E  SN',
+    'NS SS  SSSS          SN',
+    'NS  SS  SS      S    SN',
+    'NS  ESS      SSSSS  SSN',
+    'NSSSSSSSSSSTSSSSSSSSSSN',
+    'NNNNNNNNNNNNNNNNNNNNNNN'
+  ]
+  let testLocations = parseLocations(testLocationStrings)
+  let testInstance = new InstanceMap('Test Instance 0', testLocations)
+  return testInstance
 }
 
+const mapValidator = () => {
+  //make sure all the doors lead somewhere, etc.
+  return false
+}
 
 export default InstanceMap
-export {getTestInstance}
+export { getTestInstance, mapValidator }
--- db/instanceLocation.js
+++ db/instanceLocation.js
@@ -5,80 +5,84 @@
 
 // used for parsing string maps
 const LOCATION_TYPES = {
-    N:{ // Null: a non modifyable barrier at the edge of the map that
-        type: 'null',
-        walkable: false,
-        translucent: false,
-        breakable: false 
-    },         
-    B:{ // Block: a solid object which can not be walked through or seen Through
-        type: 'block',
-        walkable: false,
-        translucent: false,
-        breakable: true
-    },        
-    F:{ // Fog: can be walked through but not seen through
-        type: 'fog',
-        walkable: true,
-        translucent: false
-    },          
-    O:{ // Obstacle: can be seen through but not walked through (ie crystal, ice, brambles, rapid waters?)
-        type: 'obstacle',
-        walkable: false,
-        translucent: true,
-    },  
-    S:{ // Special: specific effect based on Location (water, lava, ice, acid, etc.)
-        type: 'special'
-    },      
-    D:{ // Door: Where the player enters or leaves from (back to town, next level of the dungeon)
-        type: 'door',
-        walkable: true,
-        translucent: false,
-        breakable: false 
-    },       
-    G: { // Ground: Where the player walks atop
-        type: 'ground',
-        walkable: true,
-        translucent: true,
-        breakable: false
-    }
-
+  N: {
+    // Null: a non modifyable barrier at the edge of the map that
+    type: 'null',
+    walkable: false,
+    translucent: false,
+    breakable: false
+  },
+  B: {
+    // Block: a solid object which can not be walked through or seen Through
+    type: 'block',
+    walkable: false,
+    translucent: false,
+    breakable: true
+  },
+  F: {
+    // Fog: can be walked through but not seen through
+    type: 'fog',
+    walkable: true,
+    translucent: false
+  },
+  O: {
+    // Obstacle: can be seen through but not walked through (ie crystal, ice, brambles, rapid waters?)
+    type: 'obstacle',
+    walkable: false,
+    translucent: true
+  },
+  S: {
+    // Special: specific effect based on Location (water, lava, ice, acid, etc.)
+    type: 'special'
+  },
+  D: {
+    // Door: Where the player enters or leaves from (back to town, next level of the dungeon)
+    type: 'door',
+    walkable: true,
+    translucent: false,
+    breakable: false,
+    destination: 'town'
+  },
+  G: {
+    // Ground: Where the player walks atop
+    type: 'ground',
+    walkable: true,
+    translucent: true,
+    breakable: false
+  }
 }
 
 let InstanceLocation = class InstanceLocation {
-    constructor ( locationTypeString )
-    {
-        let locationType = LOCATION_TYPES[ locationTypeString ]
-        if ( !locationType ) throw ( `"${ locationTypeString }" is not a valid location time >:O` )
-        
-        // some of these may be undefined for certain types 
-        //  ie. special we do not define as walkable or opaque because it changes with zone
-        this.type = locationType.type
-        this.walkable = locationType.walkable //can walk through
-        this.translucent = locationType.translucent // can see through
-        this.breakable = locationType.breakable // can be broken
-    }
+  constructor(locationTypeString) {
+    let locationType = LOCATION_TYPES[locationTypeString]
+    if (!locationType)
+      throw `"${locationTypeString}" is not a valid location time >:O`
+
+    // some of these may be undefined for certain types
+    //  ie. special we do not define as walkable or opaque because it changes with zone
+    this.type = locationType.type
+    this.walkable = locationType.walkable //can walk through
+    this.translucent = locationType.translucent // can see through
+    this.breakable = locationType.breakable // can be broken
+    if (locationType.destination) this.destination = locationType.destination
+    // descriptors
+    // encounter
+  }
 }
 
 // take an array of valid letters and convert to an array of instanceLocations
-const parseLocations = (locStringArray) =>
-{
-    let locationArray = [];    
-    locStringArray.forEach( row =>
-    {
-        let locationRow = []
-        row.forEach( locationString =>
-        {
-            let location = new InstanceLocation(locationString)
-            locationRow.push(location)
-        })
-        locationArray.push(locationRow)
-    });
-    return locationArray
+const parseLocations = (locStringArray) => {
+  let locationArray = []
+  locStringArray.forEach((row) => {
+    let locationRow = []
+    row.forEach((locationString) => {
+      let location = new InstanceLocation(locationString)
+      locationRow.push(location)
+    })
+    locationArray.push(locationRow)
+  })
+  return locationArray
 }
-
-
-
 
 export default InstanceLocation
 export { parseLocations, LOCATION_TYPES }
