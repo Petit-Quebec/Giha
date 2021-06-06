01d970dc4f61ed7a51a504f9d1cbfcb2bed54d3e
Rachel <rachel.fisher0@walmartlabs.com>
2021-06-05 22:28:05 -0700

    okay now they all pass

--- chatbot/cmds/__test__/adventure.spec.js
+++ chatbot/cmds/__test__/adventure.spec.js
@@ -1,4 +1,4 @@
-import { run } from '../adventure.js'
+import adventure from '../adventure.js'
 import { getHeroById } from '../../../Giha/heroManager.js'
 jest.mock('../../../Giha/heroManager.js')
 
@@ -22,21 +22,21 @@ describe('Adventure Test', () => {
   })
 
   it('should throw if user has no valid hero', () => {
-    expect(run(null, mockMessage, [])).rejects.toEqual(
+    expect(adventure.run(null, mockMessage, [])).rejects.toEqual(
       '1 does not have a valid hero, please make a hero with !rise <name>'
     )
   })
 
   it('should throw if too many users are tagged', () => {
     getHeroById.mockReturnValue(true)
-    expect(run(null, mockMessage, [1, 2, 3, 4, 5])).rejects.toEqual(
+    expect(adventure.run(null, mockMessage, [1, 2, 3, 4, 5])).rejects.toEqual(
       'too many users tagged'
     )
   })
 
   it('should throw if any tagged user has no hero', () => {
     getHeroById.mockReturnValueOnce(true).mockReturnValueOnce(false)
-    expect(run(null, mockMessage, [1, 2, 3, 4])).rejects.toEqual(
+    expect(adventure.run(null, mockMessage, [1, 2, 3, 4])).rejects.toEqual(
       '2 does not have a valid hero, please make a hero with !rise <name>'
     )
   })
