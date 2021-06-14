6f033fb47802c33dc862777341bd6ec2a5a461dd
Wizard of the North <intercomman14@gmail.com>
2021-06-13 18:35:47 -0700

    removed some old useless stuff

--- chatbot/lib/invalidCommand.js
+++
@@ -1,9 +0,0 @@
-const DELAY_MS = 5000
-
-export default async (msg) => {
-  let response = await msg.channel.send(
-    `\`${msg.content}\` is not a valid command\nFor help type !help`
-  )
-  response.delete(DELAY_MS)
-  return true
-}
