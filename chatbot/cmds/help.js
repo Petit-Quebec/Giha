4523f487920a847c0b2cb1a299794fc03441cc8f
Rachel <rachel.fisher0@walmartlabs.com>
2021-07-10 12:49:46 -0700

    removed js changes to check husky

--- chatbot/cmds/help.js
+++ chatbot/cmds/help.js
@@ -48,7 +48,8 @@ const run = async (bot, message) => {
     bot.commands.forEach((cmd) => {
       txt += ` !${cmd.help.name}\n`
     })
-  
+
+    msg.edit(txt + '\n```')
     log(txt, true)
   } catch (err) {
     // if there is a problem, log it and inform the user
