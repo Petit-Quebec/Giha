bb02f05bbc1a3b63aca616cb58bf01c68adb1482
Rachel <rachel.fisher0@walmartlabs.com>
2021-07-10 12:51:35 -0700

    changed a js file to test

--- chatbot/emojiManager.js
+++ chatbot/emojiManager.js
@@ -1,6 +1,6 @@
 const getEmojiByName = (bot, emojiName) => {
   let searchRes = bot.emojis.find((emoji) => emoji.name === emojiName)
-  if (!searchRes) throw `no emoji named ${emojiName}`
+  if (!searchRes) throw `no emoji named HAHAHAH ${emojiName}`
   return searchRes
 }
 export { getEmojiByName }
